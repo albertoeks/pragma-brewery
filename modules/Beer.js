adb5c11c431cf068cd1791a2fafedec62afda438
Alberto <eks.alberto@gmail.com>
2019-05-27 11:44:13 +1000

    refactor: created beerFactory

--- modules/Beer.js
+++ modules/Beer.js
@@ -1,4 +1,21 @@
-module.exports = class Beer{
+module.exports = class BeerFactory{
+    static create(type){
+        if(type == 'Pilsner')
+            return new Beer(type, 4, 6);
+        if(type == 'IPA')
+            return new Beer(type, 5, 6);
+        if(type == 'Lager')
+            return new Beer(type, 4, 7);
+        if(type == 'Stout')
+            return new Beer(type, 6, 8);
+        if(type == 'Wheat Beer')
+            return new Beer(type, 3, 5);
+        if(type == 'Pale Ale')
+            return new Beer(type, 4, 6);
+    }
+}
+
+class Beer{
     constructor(name, minDegree, maxDegree){
         this.name = name;
         this.minDegree = minDegree;
